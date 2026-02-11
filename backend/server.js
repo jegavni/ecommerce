e8a473bf63759b7eeb444015cd74366dcee140fe
jegavni <jegatheeshchandra@gmail.com>
2026-02-11 15:36:14 +0530

    Deployment ready

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,10 @@ app.use("/api/seller",sellerRoutes);
 app.use("/api/admin/products", adminProductRoutes);
 app.use("/api/payment", paymentRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.get("/cookie-test", (req, res) => {
   res.json({
     cookies: req.cookies,
@@ -62,9 +66,12 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
 // const hashed = await bcrypt.hash("jeg@1995", 10);
 // await User.updateOne(
